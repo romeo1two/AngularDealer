b651a59bdaf3afc82b86c2c92352ade99e9c1ded
romeo1two <romeo1two@gmail.com>
2018-10-17 18:16:28 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Model } from "./model";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "AngDlr-app",
+    templateUrl: "app.component.html"
 })
 export class AppComponent {
-  title = 'app';
+    model = new Model();
+    getName() {
+        return this.model.user;
+    }
+    getTodoItems() {
+        return this.model.items;
+    }
 }
